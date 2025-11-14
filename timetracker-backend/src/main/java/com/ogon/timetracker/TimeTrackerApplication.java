262e92ddfad8295e4ebd5d1bc730425c7e19184f
ghajabiramsriramuluogon <ghajabiram.sriramulu@enia.com>
2025-11-14 13:19:45 +0530

    changes

--- timetracker-backend/src/main/java/com/ogon/timetracker/TimeTrackerApplication.java
+++ timetracker-backend/src/main/java/com/ogon/timetracker/TimeTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimetrackerApplication {
+public class TimeTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimetrackerApplication.class, args);
+		SpringApplication.run(TimeTrackerApplication.class, args);
 	}
 
 }
