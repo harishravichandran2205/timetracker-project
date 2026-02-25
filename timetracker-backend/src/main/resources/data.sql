976f0d1faabb2b7da9a32b249b4720ec96e4ce83
sankiliyandigurusamyENIA <Sankiliyandi.Gurusamy@enia.com>
2026-02-25 09:29:43 +0530

    updated admin password and loader changes.

--- timetracker-backend/src/main/resources/data.sql
+++ timetracker-backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 INSERT ignore INTO app_user (id, email, first_name, last_name, password)
-VALUES (1, 'timetrackeradmin@ogondigital.com', 'Admin', 'User', '$2a$10$OKKlmW5bQzpAAHJmeBUUsOLE7l2krYD.qwdcT6u4aXFNnrF9ZfvTK');
+VALUES (1, 'timetrackeradmin@ogondigital.com', 'Admin', 'User', '$2a$10$XFY45yrS1HdMdRAdwaFVk.06DLzcv6Ie7NawzFW3a4vAXvaNI1Rsm');
 
 --
 -- Insert roles for admin user
